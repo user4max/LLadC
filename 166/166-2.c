c011c1ec75a75354ae3de5cd4355f9eb9852395a
Carp-bezverhnii MAXIM <cmaxim@e1p31.chisinau.42.fr>
2017-11-19 13:44:14 +0200

    166-2.c

---
+++ 166/166-2.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+
+int main()
+{
+	int nb = 5;
+	int row, column;
+
+	row = 1;
+	while (row <= nb)
+	{
+		column = 1;
+		while (column <= row)
+		{
+			write(1, "* ", 1);
+			column++;
+		}
+		write(1, "\n", 1);
+		row++;
+	}
+}
